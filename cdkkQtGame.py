14b2b84732b3d5a4a75408c21ef8dbe1805498ca
BrianDunneKK <Brian.Dunne.KK@gmail.com>
2023-03-22 22:39:40 +0000

    Add splash screen

--- cdkkQtGame.py
+++ cdkkQtGame.py
@@ -1,9 +1,11 @@
 import typing
-from PyQt6.QtCore import (Qt, QSize, QPoint, QRect)
-from PyQt6.QtGui import (QPainter, QPainter, QPen, QBrush, QPaintEvent, QMouseEvent, QResizeEvent)
+from PyQt6.QtCore import (Qt, QSize, QPoint, QRect, QTimer)
+from PyQt6.QtGui import (QPainter, QPainter, QPen, QBrush, QPaintEvent, QMouseEvent, QResizeEvent, QPixmap, QFont)
 from PyQt6.QtWidgets import (
-    QApplication, QMainWindow, QGridLayout, QWidget, QLabel, QGraphicsEllipseItem,
-    QGraphicsLineItem, QGraphicsRectItem, QGraphicsScene, QGraphicsView)
+    QApplication, QMainWindow, QVBoxLayout, QGridLayout, QWidget, QLabel, QPushButton, QCheckBox, QGraphicsEllipseItem,
+    QGraphicsLineItem, QGraphicsRectItem, QGraphicsScene, QGraphicsView, QSplashScreen, QDialog)
+
+from cdkkGame import Game
 
 # ----------------------------------------
 
@@ -13,9 +15,32 @@ class cdkkQtGame(QApplication):
         self.game = Game()
 
     def init(self, title : str = "") -> bool:
-        self.window = GameWindow(title)
-        self.window.init()
+        self.window = QMainWindow()
         return True
 
 # ----------------------------------------
 
+class cdkkSplashScreen:
+    filename : str
+    splash : QSplashScreen
+    win :QWidget
+
+    @staticmethod
+    def init(filename : str, win : QWidget, show : bool, close_timeout : int):
+        cdkkSplashScreen.win = win
+        pix = QPixmap(filename)
+        cdkkSplashScreen.splash = QSplashScreen(pix)
+        if show:
+            cdkkSplashScreen.show()
+        if close_timeout > 0:
+            QTimer.singleShot(close_timeout, cdkkSplashScreen.close)
+
+    @staticmethod
+    def show():
+        cdkkSplashScreen.splash.show()
+
+    @staticmethod
+    def close(filename : str = ""):
+        cdkkSplashScreen.splash.finish(cdkkSplashScreen.win)
+
+# ----------------------------------------
